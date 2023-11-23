351461df9a778dcbba6eea6a7141d5643089c1be
Billy <billychorey@gmail.com>
2023-11-23 08:57:41 -0500

    unit tests lab

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,6 @@
-// Your code here
 function isPalindrome(word) {
-  }
-  
-  module.exports = { isPalindrome };function isPalindrome(word) {
     const lowerCaseWord = word.toLowerCase();
-    
     const reversedWord = lowerCaseWord.split('').reverse().join('');
-  
     return lowerCaseWord === reversedWord;
   }
   
