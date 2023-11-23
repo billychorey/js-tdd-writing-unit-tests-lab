46d8fc77e108d1bf6413692303478db767265d05
Billy <billychorey@gmail.com>
2023-11-22 21:33:36 -0500

    still not a confidence inspiring learning lesson

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,13 @@
 // Your tests here
+const { isPalindrome } = require('../utils');
+
+describe('isPalindrome', () => {
+  it('returns true for a palindrome word', () => {
+    expect(isPalindrome('racecar')).toBe(true);
+  });
+
+  it('returns false for a non-palindrome word', () => {
+    expect(isPalindrome('car')).toBe(false);
+  });
+
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+function isPalindrome(word) {
+  }
+  
+  module.exports = { isPalindrome };function isPalindrome(word) {
+    const lowerCaseWord = word.toLowerCase();
+    
+    const reversedWord = lowerCaseWord.split('').reverse().join('');
+  
+    return lowerCaseWord === reversedWord;
+  }
+  
+  module.exports = { isPalindrome };
